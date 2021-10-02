01fb7a876297adbe5b2ba7f2e91531aeeaa48798
WILLIAM DE SA RODRIGUES <wsr2k2@gmail.com>
2021-10-02 20:02:32 -0300

    melhorias no projeto Pokedex

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+
 
 let pokedex = [
   {
     numero: 01,
     nome: "Blastoise",
     tipo: "Water",
-    imagem: "01.png",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/009.png",
     descricao: "It crushes its foe under its heavy body to cause fainting. In a pinch, it will withdraw inside its shell.",
     altura: "1.6 m",
     peso: "85.5 kg",
@@ -19,7 +23,7 @@ let pokedex = [
     numero: 02,
     nome: "Haunter",
     tipo: "Ghost, Poison",
-    imagem: "02.png",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/093.png",
     descricao: "Its tongue is made of gas. If licked, its victim starts shaking constantly until death eventually comes.",
     altura: "1.6 m",
     peso: "0.1 kg",
@@ -30,7 +34,7 @@ let pokedex = [
     numero: 03,
     nome: "Psyduck",
     tipo: "Water",
-    imagem: "03.png",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/054.png",
     descricao: "Psyduck is constantly beset by headaches. If the Pokémon lets its strange power erupt, apparently the pain subsides for a while.",
     altura: "0.8 m",
     peso: "19.6 kg",
@@ -39,37 +43,58 @@ let pokedex = [
   },
 ];
 
-let message = "";
-
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded());
+// let message = "";
 
 
 app.get("/", (req, res) => {
-  setTimeout(() => {
-    message = "";
-  }, 1000);
-  res.render("index", {pokedex, message}); // Nome do arquivo, o EJS já busca dentro da pasta views.
+  res.render("index", { titulo: "POKEDEX", pokedex: pokedex });  
 });
 
 app.get("/detalhes/:id", (req, res) => {
-  const id = req.params.id;
-  const pokemon = pokedex[id];
-  res.render("detalhes", {
-    pokemon,
-  });
+  const id = req.params.id
+  const pokemon = pokedex[id]
+  res.render("detalhes.ejs", { pokemon })
+});
+
+app.get("/cadastro", (req, res) => {
+  res.render("cadastro.ejs")
 })
 
-app.get("/cadastro", (req, res) => {  
-  res.render("cadastro");
-});
-
-app.post("/cadastro", (req, res) => {
-  const pokemon = req.body;
-  pokedex.push(pokemon);
+app.post("/new", (req, res) => {
+  const {nome, tipo, imagem, descricao, altura, peso, categoria, habilidade} = req.body;
+  const novoPokemon = {
+    nome: nome,
+    tipo: tipo,
+    imagem: imagem,
+    descricao: descricao,
+    altura: altura,
+    peso: peso,
+    categoria: categoria,
+    habilidade: habilidade
+  }
+  pokedex.push(novoPokemon);
   res.redirect("/");
-});
+
+})
+
+// app.get("/cadastro", (req, res) => {
+//   res.render("cadastro.ejs");
+// })
+
+// app.get("/detalhes", (req, res) => {  
+//   res.render("detalhes");
+// });
+
+
+// app.get("/cadastro", (req, res) => {  
+//   res.render("cadastro");
+// });
+
+// app.post("/cadastro", (req, res) => {
+//   const pokemon = req.body;
+//   pokedex.push(pokemon);
+//   res.redirect("/");
+// });
 
 app.listen(port, () =>
   console.log(`Servidor rodando em http://localhost:${port}`)
